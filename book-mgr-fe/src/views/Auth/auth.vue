b92bc5e9c444d37d7f804fa9e9da66694405f810
阿威 <2279085675@qq.com>
2021-11-29 10:48:22 +0800

    完成大部分认证逻辑

--- book-mgr-fe/src/views/Auth/auth.vue
+++ book-mgr-fe/src/views/Auth/auth.vue
@@ -12,14 +12,22 @@
       <a-tabs v-model:activeKey="activeKey">
         <a-tab-pane tab="登录" key="1">
           <div class="item">
-            <a-input size="large" placeholder="账户">
+            <a-input
+              size="large"
+              placeholder="账户"
+              v-model:value="loginFrom.account"
+            >
               <template #prefix>
                 <UserOutlined />
               </template>
             </a-input>
           </div>
           <div class="item">
-            <a-input size="large" placeholder="密码">
+            <a-input
+              size="large"
+              placeholder="密码"
+              v-model:value="loginFrom.password"
+            >
               <template #prefix>
                 <LockOutlined />
               </template>
@@ -29,7 +37,9 @@
             <a href="">忘记密码</a>
           </div>
           <div class="item">
-            <a-button size="large" type="primary"> 登入 </a-button>
+            <a-button size="large" type="primary" @click="login">
+              登入
+            </a-button>
           </div>
         </a-tab-pane>
         <a-tab-pane tab="注册" key="2">
@@ -56,7 +66,11 @@
             </a-input>
           </div>
           <div class="item">
-            <a-input size="large" placeholder="邀请码">
+            <a-input
+              size="large"
+              placeholder="邀请码"
+              v-model:value="regFrom.inviteCode"
+            >
               <template #prefix>
                 <MailOutlined />
               </template>
@@ -76,6 +90,8 @@
 <script>
 import { ref, reactive } from "vue";
 import { auth } from "../../service/inedx";
+import { result } from "../../helper/utils/index";
+import { message } from "ant-design-vue";
 import {
   UserOutlined,
   LockOutlined,
@@ -88,17 +104,68 @@ export default {
     MailOutlined,
   },
   setup() {
+    //注册用的表单数据
     const regFrom = reactive({
       account: "",
       password: "",
+      inviteCode: "",
     });
-    const register = () => {
-      auth.register(regFrom.account, regFrom.password);
+    //注册逻辑
+    const register = async () => {
+      if (regFrom.account === "") {
+        message.info("请输入账号");
+        return;
+      }
+      if (regFrom.password === "") {
+        message.info("请输入密码");
+        return;
+      }
+      if (regFrom.inviteCode === "") {
+        message.info("请输入邀请码");
+        return;
+      }
+      const res = await auth.register(
+        regFrom.account,
+        regFrom.password,
+        regFrom.inviteCode
+      );
+      result(res).success((data) => {
+        message.success(data.msg);
+      });
+    };
+
+    //登陆用的表单逻辑
+    const loginFrom = reactive({
+      account: "",
+      password: "",
+    });
+    //登录逻辑
+    const login = async () => {
+      if (loginFrom.account === "") {
+        message.info("请输入账号");
+        return;
+      }
+      if (loginFrom.password === "") {
+        message.info("请输入密码");
+        return;
+      }
+
+      const res = await auth.login(loginFrom.account, loginFrom.password);
+
+      result(res).success((data) => {
+        message.success(data.msg);
+      });
     };
     return {
       activeKey: ref("1"),
+
+      //注册相关的数据
       regFrom,
       register,
+
+      //登录相关的数据
+      loginFrom,
+      login,
     };
   },
 };
